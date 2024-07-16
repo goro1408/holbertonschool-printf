8c5a4478d2b24afb7eb1762cafb653e95128df1e
Orlando <goro.14082@gmail.com>
2024-07-16 19:14:21 -0400

    printf

--- tests/main.c
+++ tests/main.c
@@ -3,7 +3,8 @@
 #include "main.h"
 
 /**
- * main - Entry point for testing _printf
+ * main - Entry point
+ *
  * Return: Always 0
  */
 int main(void)
@@ -32,7 +33,7 @@ int main(void)
     _printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
     _printf("Address:[%p]\n", addr);
-    printf("Address:[%p]\n", addr);
+     printf("Address:[%p]\n", addr);
     len = _printf("Percent:[%%]\n");
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
